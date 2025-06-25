41db03323dc7b4e9e7a2abd5fa88a9b9b95aa4e8
Muzahir Ali <muzahiralig@gmail.com>
2025-06-25 18:54:39 -0400

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+sum_of_num = num1 + num2
+
+print("sum_of_2numbers:",sum_of_num)
+
+diff_of_num = num1 - num2
+print("diff_of_2numbers:",diff_of_num)
+
+product_of_2num = num1 * num2
+print("product_of_2numbers", product_of_2num)
+
+divison_of_2numbers = num1 / num2
+print("divison_of_2numbers: ", divison_of_2numbers)
